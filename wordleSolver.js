56eae337b1d798dba6737d7e64b77c8da0f9c811
dizmond <dizmond@live.unc.edu>
2022-03-13 23:03:42 -0400

    added changeColor() function

--- wordleSolver.js
+++ wordleSolver.js
@@ -1,11 +1,31 @@
-var letters = [];
+var letters = [
+    [ , , , , ],
+    [ , , , , ],
+    [ , , , , ],
+    [ , , , , ],
+    [ , , , , ]
+];
+const word = ["T","O","D","A","Y"];
+var clickCount = 0;
 
-function addWord(input) {
-
-    letters.push(input);
-
+function addWord(input,id, row, column) {
+    letters[row][column] = input;
+    colorChange(input,id,column);
 }
 
 function showLetters() {
-    console.log(letters.join(''))
+    console.log(letters.join(''));
+}
+
+function colorChange(input,id, column) {
+
+    if (input == word[column]){
+        var v = document.getElementById(id);
+        v.style.backgroundColor = "green";
+    }
+    else if ((input != word[column]) && (word.includes(input))){
+        var v = document.getElementById(id);
+        v.style.backgroundColor = "yellow";
+    }
+    
 }
