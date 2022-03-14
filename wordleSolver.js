8d290163be119550d3da329d0357742b3b228fb5
dizmond <dizmond@live.unc.edu>
2022-03-14 00:25:05 -0400

    changed it to be button dependent & added function to entire grid

--- wordleSolver.js
+++ wordleSolver.js
@@ -3,29 +3,41 @@ var letters = [
     [ , , , , ],
     [ , , , , ],
     [ , , , , ],
+    [ , , , , ],
     [ , , , , ]
+];
+
+var idLabel = [
+    ["00","01","02","03","04"],
+    ["10","11","12","13","14"],
+    ["20","21","22","23","24"],
+    ["30","31","32","33","34"],
+    ["40","41","42","43","44"],
+    ["50","51","52","53","54"]
 ];
 const word = ["T","O","D","A","Y"];
 var clickCount = 0;
 
 function addWord(input,id, row, column) {
     letters[row][column] = input;
-    colorChange(input,id,column);
 }
 
 function showLetters() {
-    console.log(letters.join(''));
-}
-
-function colorChange(input,id, column) {
-
-    if (input == word[column]){
-        var v = document.getElementById(id);
-        v.style.backgroundColor = "green";
-    }
-    else if ((input != word[column]) && (word.includes(input))){
-        var v = document.getElementById(id);
-        v.style.backgroundColor = "yellow";
-    }
-    
+    if (clickCount < 6) { //row
+        for (var i = 0; i < word.length; i++) { //column
+            if (letters[clickCount][i] == word[i]) {
+                var v = document.getElementById(idLabel[clickCount][i]);
+                v.style.backgroundColor = "green";
+            }
+            else if ((letters[clickCount][i] != word[i]) && (word.includes(letters[clickCount][i]))) {
+                var v = document.getElementById(idLabel[clickCount][i]);
+                v.style.backgroundColor = "yellow";
+            }
+            else {
+                var v = document.getElementById(idLabel[clickCount][i]);
+                v.style.backgroundColor = "gray";
+            }
+        }        
+        clickCount = clickCount + 1;
+    }   
 }
